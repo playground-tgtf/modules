0e6113cf608eab061f101a882b6949c1f80f41e6
playground-tgtf <sureshap@yahoo.com>
2025-06-16 06:51:57 +1000

    Update output.tf
    updated output.tf file

--- s3/output.tf
+++ s3/output.tf
@@ -1,6 +1,6 @@
 output "s3_bucket_id" {
   description = "The name of the bucket."
-  value       = try(aws_s3_bucket.this[0].id, aws_s3_directory_bucket.this[0].bucket, "")
+  value       = aws_s3_bucket.this[0].id
 }
 
 output "s3_bucket_arn" {
